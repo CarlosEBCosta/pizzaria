215cf7616857a40ded5ffe47000f2044290b23fc
cebccosta <eduardocosta35@hotmail.com>
2023-02-13 19:36:31 -0300

    pizzaria

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Mussarela,tomate, azeitona, orégano'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Calabresa, mussarela, tomate, orégano'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Mussarela, catupiri, parmesão, provolone'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Mussarela, tomate, rúcula, brócolis azeitona'},
+    {id:5, name:'Marguerita', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Mussarela, tomate, manjericão'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Mussarela, tomate, azeitona, manjericão, bacon'},
+   
+];
